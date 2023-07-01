d009fb86fab913147e4355c16c637dc611cd2d45
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-01 11:21:38 +0300

    add variables

--- DeliveryDriver/Assets/Driver.cs
+++ DeliveryDriver/Assets/Driver.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+  float steerSpeed = 1;
+  float moveSpeed = 0.01f;
+
+
   void Start()
   {
 
@@ -11,7 +15,7 @@ public class Driver : MonoBehaviour
 
   void Update()
   {
-    transform.Rotate(0, 0, 0.1f);
-    transform.Translate(0, 0.01f, 0);
+    transform.Rotate(0, 0, steerSpeed);
+    transform.Translate(0, moveSpeed, 0);
   }
 }
