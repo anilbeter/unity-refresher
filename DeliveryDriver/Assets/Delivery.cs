74247dd63198d6e93c7e1814857238e15849aca4
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-07-02 12:54:10 +0300

    add boolean logic

--- DeliveryDriver/Assets/Delivery.cs
+++ DeliveryDriver/Assets/Delivery.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+  bool hasPackage;
+
   void OnCollisionEnter2D(Collision2D other)
   {
     Debug.Log("Ouch!");
@@ -13,10 +15,12 @@ public class Delivery : MonoBehaviour
     if (other.tag == "Package")
     {
       Debug.Log("You got the package!");
+      hasPackage = true;
     }
-    else if (other.tag == "Customer")
+    if (other.tag == "Customer" && hasPackage)
     {
       Debug.Log("Package delivered!");
+      hasPackage = false;
     }
   }
 }
