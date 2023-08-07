614c888e733a46604a9448ce3f5d9919b0a49666
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-07 11:26:05 +0300

    add transform.Rotate() method

--- ADR/Assets/Scripts/Driver.cs
+++ ADR/Assets/Scripts/Driver.cs
@@ -7,12 +7,14 @@ public class Driver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        // transform.Rotate(0, 0, -45);
+        // oyun başlar başlamaz z aksisinde -45 derece dönücek
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        transform.Rotate(0, 0, 0.1F);
+        // her frame de 0.1 derece dönücek
     }
 }
