820e8d46364857bd81eb42107cf29a83f11345ad
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-18 10:07:51 +0300

    add public method that GetQuestion()

--- QUIZ ADR/Assets/Scripts/QuestionSO.cs
+++ QUIZ ADR/Assets/Scripts/QuestionSO.cs
@@ -7,4 +7,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2, 6)]
     [SerializeField] string question = "Enter a new question here";
+
+    public string GetQuestion()
+    {
+        return question;
+    }
+}
+
+public class Test
+{
+    readonly QuestionSO questionSO;
+
+    void TestA()
+    {
+        string questionText = questionSO.GetQuestion();
+        // Görüldüğü gibi başka bir class'dan public olan GetGuestion() methoduma erişim sağlayabiliyorum, çünkü public yaptım.
+    }
 }
