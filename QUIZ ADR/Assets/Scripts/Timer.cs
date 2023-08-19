31c5d8ee7129c7ead5a37f666b15b9008916288a
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-19 12:46:34 +0300

    add float variables

--- QUIZ ADR/Assets/Scripts/Timer.cs
+++ QUIZ ADR/Assets/Scripts/Timer.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    [SerializeField] float timeToCompleteQuestion = 30f;
+    [SerializeField] float timeToShowCorrectAnswer = 10f;
+    float timerValue;
     void Update()
     {
 
