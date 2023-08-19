1ee5d42295a36d1662783b1944f0ebcaf17e9c4b
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-19 12:25:43 +0300

    edit if else statement

--- QUIZ ADR/Assets/Scripts/Quiz.cs
+++ QUIZ ADR/Assets/Scripts/Quiz.cs
@@ -21,31 +21,21 @@ public class Quiz : MonoBehaviour
     public void OnAnswerSelected(int index)
     {
         Image buttonImage;
-        Image falseImg;
-        TextMeshProUGUI falseText;
 
         if (index == question.GetCorrectAnswerIndex())
         {
-            questionText.text = "Doğru!!";
+            questionText.text = "Doğru!";
             buttonImage = answerButtons[index].GetComponent<Image>();
-            // buttonImage.color = new Color(0, 1, 0, (float)0.2);
-            TextMeshProUGUI btnText = answerButtons[index].GetComponentInChildren<TextMeshProUGUI>();
-            btnText.color = Color.white;
-            btnText.fontWeight = FontWeight.Bold;
             buttonImage.sprite = correctAnswerSprite;
         }
         else
         {
             correctAnswerIndex = question.GetCorrectAnswerIndex();
             string correctAnswer = question.GetAnswer(correctAnswerIndex);
-            questionText.text = "Aklın nerde bro doğru cevap:\n" + correctAnswer;
+            questionText.text = "Yanlış cevap bro aklın nerde? Doğru cevap;\n" + correctAnswer;
             buttonImage = answerButtons[correctAnswerIndex].GetComponent<Image>();
             buttonImage.sprite = correctAnswerSprite;
-            falseImg = answerButtons[index].GetComponent<Image>();
-            falseImg.color = new Color(1, 0, 0, (float)0.5);
-            falseText = answerButtons[index].GetComponentInChildren<TextMeshProUGUI>();
-            falseText.color = Color.white;
-            falseText.fontWeight = FontWeight.Bold;
+
         }
         SetButtonState(false);
         // butona bastıktan sonra butonların devre dışı kalmasını sağlıyor
