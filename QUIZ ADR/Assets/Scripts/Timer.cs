97b6635532cd8055a2ce50c324fdb65d469ad5b6
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-19 13:46:07 +0300

    add UpdateTime method

--- QUIZ ADR/Assets/Scripts/Timer.cs
+++ QUIZ ADR/Assets/Scripts/Timer.cs
@@ -7,8 +7,32 @@ public class Timer : MonoBehaviour
     [SerializeField] float timeToCompleteQuestion = 30f;
     [SerializeField] float timeToShowCorrectAnswer = 10f;
     float timerValue;
+
+    public bool isAnsweringQuestion = false;
     void Update()
     {
 
+    }
+
+    void UpdateTimer()
+    {
+        timerValue -= Time.deltaTime;
+
+        if (isAnsweringQuestion)
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = false;
+                timerValue = timeToShowCorrectAnswer;
+            }
+        }
+        else
+        {
+            if (timerValue <= 0)
+            {
+                isAnsweringQuestion = true;
+                timerValue = timeToCompleteQuestion;
+            }
+        }
     }
 }
