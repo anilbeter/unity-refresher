ec618af1d2604503d6b2b35696fb58f987f11521
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-20 09:27:22 +0300

    create CalculateScore method

--- QUIZ ADR/Assets/Scripts/ScoreKeeper.cs
+++ QUIZ ADR/Assets/Scripts/ScoreKeeper.cs
@@ -22,4 +22,9 @@ public class ScoreKeeper : MonoBehaviour
         questionsSeen++;
     }
 
+    public int CalculateScore()
+    {
+        return Mathf.RoundToInt(correctAnswers / (float)questionsSeen * 100);
+    }
+
 }
