b931efca13acbc06ba20b412d134ab34dac0bf9f
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-20 19:12:15 +0300

    replace awake() function instead start() and finish off game

--- QUIZ ADR/Assets/Scripts/GameManager.cs
+++ QUIZ ADR/Assets/Scripts/GameManager.cs
@@ -8,11 +8,15 @@ public class GameManager : MonoBehaviour
     Quiz quiz;
     EndScreen endScreen;
 
-    void Start()
+    private void Awake()
     {
         quiz = FindObjectOfType<Quiz>();
         endScreen = FindObjectOfType<EndScreen>();
+    }
 
+
+    void Start()
+    {
         quiz.gameObject.SetActive(true);
         endScreen.gameObject.SetActive(false);
     }
--- QUIZ ADR/Assets/Scripts/Quiz.cs
+++ QUIZ ADR/Assets/Scripts/Quiz.cs
@@ -15,7 +15,7 @@ public class Quiz : MonoBehaviour
     [Header("Answers")]
     [SerializeField] GameObject[] answerButtons;
     int correctAnswerIndex;
-    bool hasAnsweredEarly;
+    bool hasAnsweredEarly = true;
 
     [Header("Sprites")]
     [SerializeField] Sprite defaultAnswerSprite;
@@ -34,7 +34,7 @@ public class Quiz : MonoBehaviour
 
     public bool isComplete;
 
-    void Start()
+    void Awake()
     {
         timer = FindObjectOfType<Timer>();
         scoreKeeper = FindObjectOfType<ScoreKeeper>();
@@ -48,6 +48,12 @@ public class Quiz : MonoBehaviour
         timerImage.fillAmount = timer.fillFraction;
         if (timer.loadNextQuestion)
         {
+            if (progressBar.value == progressBar.maxValue)
+            {
+                isComplete = true;
+                return;
+            }
+
             hasAnsweredEarly = false;
             GetNextQuestion();
             timer.loadNextQuestion = false;
@@ -68,11 +74,6 @@ public class Quiz : MonoBehaviour
 
         timer.CancelTimer();
         scoreText.text = "Score: " + scoreKeeper.CalculateScore() + "%";
-
-        if (progressBar.value == progressBar.maxValue)
-        {
-            isComplete = true;
-        }
     }
 
     void DisplayAnswer(int index)
