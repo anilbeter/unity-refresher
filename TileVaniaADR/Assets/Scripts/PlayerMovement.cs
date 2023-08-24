e29a271123df46ba64d90d5cac852adcaa91d006
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-24 14:19:19 +0300

    add Vector2 variable and OnMove method

--- TileVaniaADR/Assets/Scripts/PlayerMovement.cs
+++ TileVaniaADR/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Vector2 moveInput;
+
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+        Debug.Log(moveInput);
     }
 }
