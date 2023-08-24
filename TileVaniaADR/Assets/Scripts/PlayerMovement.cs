c1a5dc24d56b8740a17e6bd00c51fab1826cdb6a
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-24 14:32:00 +0300

    add horizontal player movement

--- TileVaniaADR/Assets/Scripts/PlayerMovement.cs
+++ TileVaniaADR/Assets/Scripts/PlayerMovement.cs
@@ -3,24 +3,33 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngineInternal;
 
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
+    Rigidbody2D myRigidbody2D;
+    [SerializeField] float runSpeed = 10f;
 
     void Start()
     {
-
+        myRigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-
+        Run();
     }
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new(moveInput.x * runSpeed, myRigidbody2D.velocity.y);
+        myRigidbody2D.velocity = playerVelocity;
     }
 }
