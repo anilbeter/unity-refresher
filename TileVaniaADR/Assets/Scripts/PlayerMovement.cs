94f6b1750e89bcdb4d3375cb894ef91ed7ec8367
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-08-24 15:19:44 +0300

    avoid multi jumping

--- TileVaniaADR/Assets/Scripts/PlayerMovement.cs
+++ TileVaniaADR/Assets/Scripts/PlayerMovement.cs
@@ -12,11 +12,13 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float runSpeed = 10f;
     [SerializeField] float jumpSpeed = 5f;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
 
     void Start()
     {
         myRigidbody2D = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
     void Update()
@@ -33,6 +35,10 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            return;
+        }
         if (value.isPressed)
         {
             myRigidbody2D.velocity += new Vector2(0f, jumpSpeed);
